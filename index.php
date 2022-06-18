0ed448c5f1ab0fbbdd180e06ac3f5598a31dc595
Claudia Lisboa <ccalumby@gmail.com>
2022-06-18 17:57:47 -0400

    updated index.php

--- index.php
+++ index.php
@@ -1,29 +1,87 @@
 <?php
 require_once('./Sphere.cls.php');
+require_once('./Cylindre.cls.php');
+require_once('./PaveDroit.cls.php');
+require_once('./Cube.cls.php');
+
+// Tableau avec deux instances de chacune des classes concrètes (8 objets en tout).
+$formes = array(
+    "sphere1" => new Sphere(2),
+    "sphere2" => new Sphere(4),
+    "cylindre1" => new Cylindre(2.25, 15.33),
+    "cylindre2" => new Cylindre(4, 10),
+    "pavedroit1" => new PaveDroit(1, 4, 9),
+    "pavedroit2" => new PaveDroit(16, 4, 6),
+    "cube1" => new Cube(6),
+    "cube2" => new Cube(3)
+);
+
+// Cette fonction utilise la méthode comparerAvec de $forme1 pour la comparer avec $forme2.
+function comparerFormes($forme1, $forme2)
+{
+    $result = $forme1->comparerAvec($forme2);
+    return $result;
+    // if ($result < 0) {
+    //     echo "La forme " . $forme1 . " est plus grande que la forme " . $forme2 . ".<br/>";
+    // } elseif ($result > 0) {
+    //     echo "La forme " . $forme1 . " est plus petite que la forme " . $forme2 . ".<br/>";
+    // } else {
+    //     echo "Les formes " . $forme1 . " et " . $forme2 . " sont considerées égales.<br/>";
+    // }
+}
+
+// function comparerFormes($forme1, $forme2)
+// {
+//     global $formes;
+
+//     $result = $formes[$forme1]->comparerAvec($formes[$forme2]);
+
+//     // if ($result < 0) {
+//     //     echo "La forme " . $forme1 . " est plus grande que la forme " . $forme2 . ".<br/>";
+//     // } elseif ($result > 0) {
+//     //     echo "La forme " . $forme1 . " est plus petite que la forme " . $forme2 . ".<br/>";
+//     // } else {
+//     //     echo "Les formes " . $forme1 . " et " . $forme2 . " sont considerées égales.<br/>";
+//     // }
+// }
 ?>
 <html>
 
 <head>
     <meta charset='utf-8'>
-    <title> Hierarchy of class representing various 3D shapes</title>
+    <title>Exercice de classe #1</title>
 </head>
 
 <body>
-    <h1>PHP - class</h1>
-    <h2></h2>
+    <h1>582-31B-MA - Programmation Web avancée – Groupe 21621</h1>
+    <h2>Exercice de classe #1</h2>
     <?php
-    $sphere1 = new Sphere(2);
-    echo $sphere1 . "<br/>";
+    echo 'a) Contenu du tableau $formes:<br/>';
+    echo "<ul>";
+    foreach ($formes as $key => $value) {
+        echo "<li>" . $key . " => " . $value . "</li>";
+    }
+    echo "</ul>";
 
-    $sphere2 = new Sphere(4);
-    echo $sphere2 . "<br/>";
+    echo "<br/>";
+    echo 'b) 3 comparaisons de formes différentes:<br/>';
+    // comparerFormes("sphere1", "sphere2");
+    // comparerFormes("cylindre1", "cylindre2");
+    // comparerFormes("cube1", "cube1");
 
-    $sphere3 = new Sphere(5);
-    echo $sphere3 . "<br/>";
+    echo "<br/>";
+    echo 'c) Tableau $formes trié en ordre descendante:<br/>';
+    // On trie $formes en ordre descendant avec la fonction de comparaison comparerFormes.
+    uasort($formes, 'comparerFormes');
 
-    $sphere4 = new Sphere(6);
-    echo $sphere4 . "<br/>";
+    // On affice le tableau trié. On montre aussi la valeur
+    // de la superficie plus le volume de chaque forme
+    // pour montrer que le tableau est bien trié.
+    echo "<ul>";
+    foreach ($formes as $key => $value) {
+        echo "<li>" . $key . " => " . $value . " [DEBUG: superficie + volume = " . ($value->obtenirSuperficie() + $value->obtenirVolume()) . "]</li>";
+    }
+    echo "</ul>";
     ?>
 </body>
-
 </html>
