6b0ef32bb9a8962fa958633cca85f3302481cd99
Claudia Lisboa <ccalumby@gmail.com>
2022-06-18 16:10:32 -0400

    updated class sphere

--- Sphere.cls.php
+++ Sphere.cls.php
@@ -4,13 +4,13 @@ require_once('./Forme.cls.php');
 ini_set('display_errors','1');
 ini_set('display_startup_errors','1');
 error_reporting(E_ALL);
+
 class Sphere extends Forme{
 
     /**
      * Properties Sphere
      */ 
     private $rayon;
-
     /**
      * Constructeur de la class Sphere
      */
@@ -19,7 +19,6 @@ class Sphere extends Forme{
         parent::__construct();
         $this->rayon = $rayon; 
     }
-
     /**
      * Methodes de la class Sphere
      */
@@ -27,24 +26,28 @@ class Sphere extends Forme{
     {
         return $this->rayon;
     }
-
     public function obtenirTypeForme ()
     {
         return get_class($this); 
     }
      public function obtenirSuperficie ()
     {
-
+        $superficie = 4 * pi() * pow($this->rayon, 2);
+        return round($superficie, 2);
     }
-
      public function obtenirVolume()
      {
-
+        $volume = (4/3) * pi() * pow($this->rayon, 3);
+        return round($volume, 2);
      }
-
      public function __toString()
      {
-         
+        return $this->obtenirTypeForme()
+        ." , id = " . $this->id
+        ." , superficie = " . $this->obtenirSuperficie()
+        ." , volume = " . $this->obtenirVolume()
+        ." , rayon = " . $this->getRayon()
+        ."." ;
      }
 }
 ?>
